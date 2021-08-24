98ab19b2564ee66ac773b3491784829ec5c12d21
Heino <heinoduplessis@gmail.com>
2021-08-24 20:26:17 +0200

    8-24-21 8:26pm

--- beginning_react/src/App.js
+++ beginning_react/src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { Component } from 'react';
-import Products from './Products';
-import { Button } from 'react-bootstrap';
-import Rating from './Rating';
-
+import UserForm from './UserForm';
 
 class App extends Component {
-  render() {
-    const isValid = true;
+  render() {        
     return (
       <div>
-        <h1>My First React App!</h1>
-        <Products />
-        <Rating rating="1" />
-        <Rating rating="2" />
-        <Rating rating="3" />
-        <Rating rating="4" />
-        <Rating rating="5" />
-        <Button variant="primary" disabled={!isValid}>Default</Button>
+        <UserForm />        
+                
       </div>
     );
   }
 }
+
 export default App;
+
