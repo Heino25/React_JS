3ff3c5d026cfdb2e8cc660bfee196276bc7bfae7
Heino <heinoduplessis@gmail.com>
2021-08-26 14:44:01 +0200

    08-26-21 2:43pm

--- beginning_react/src/App.js
+++ beginning_react/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import UserForm from './UserForm';
+import GitHub from './GitHub';
 
 class App extends Component {
   render() {        
     return (
       <div>
-        <UserForm />        
+        <GitHub />       
                 
       </div>
     );
