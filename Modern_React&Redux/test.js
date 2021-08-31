b1497aa229b2e61c922050ef00efe9178ad6039a
Heino <heinoduplessis@gmail.com>
2021-08-31 19:56:15 +0200

    31-8-21

--- Modern_React&Redux/test.js
+++ Modern_React&Redux/test.js
@@ -1,19 +1,29 @@
-import "./styles.css";
-import React, { useState } from "react";
-import Field from './components/field';
-import Languages from './components/languages';
-import Translate from './components/translate';
+// import "./styles.css";
+// import React, { useState } from "react";
+// import Field from './components/field';
+// import Languages from './components/languages';
+// import Translate from './components/translate';
 
-export default function App() {
-  const [language, setLanguage] = useState('ru');
-  const [text, setText] = useState('');
+// export default function App() {
+//   const [language, setLanguage] = useState('ru');
+//   const [text, setText] = useState('');
 
-  return (
-    <div>
-      <Field label ="Enter English" onChange={setText} value={text} />
-      <Languages language={language} onLanguageChange={setLanguage} />
-      <hr />
-      <Translate text={text} language={language} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Field label ="Enter English" onChange={setText} value={text} />
+//       <Languages language={language} onLanguageChange={setLanguage} />
+//       <hr />
+//       <Translate text={text} language={language} />
+//     </div>
+//   );
+// }
+// //
+
+a = (51)
+b = (72)
+
+sum1 = ( a + b )
+
+print(sum1);
+
+
