4c34a6655977b3721cf2665c34b3b2114b696c4c
Heino <heinoduplessis@gmail.com>
2021-09-06 15:33:10 +0200

    06/09/21

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,14 +1,20 @@
 // Import the React and ReactDOM libraries
-import React from  'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Creat a react cmponent
 const App = () => {
-    return <div>Hi there!</div>;
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text" />
+            <button style="background-color: blue; color: white;">Submit</button>
+        </div>
+    );
 };
 
 // Take the react component and show it on the screen
 ReactDOM.render(
     <App />,
-        document.querySelector('#root')
+    document.querySelector('#root')
 );
