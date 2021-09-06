33426b4ec58d0d3bda653e2dc132a9fc4e789c18
Heino <heinoduplessis@gmail.com>
2021-09-06 08:32:34 +0200

    06/09/21

--- Modern_React&Redux/test.js
+++ Modern_React&Redux/test.js
@@ -1,33 +1,22 @@
-// import "./styles.css";
-// import React, { useState } from "react";
-// import Field from './components/field';
-// import Languages from './components/languages';
-// import Translate from './components/translate';
+import "./styles.css";
+import React, { useState } from "react";
+import Field from './components/field';
+import Languages from './components/languages';
+import Translate from './components/translate';
 
-// export default function App() {
-//   const [language, setLanguage] = useState('ru');
-//   const [text, setText] = useState('');
+export default function App() {
+  const [language, setLanguage] = useState('ru');
+  const [text, setText] = useState('');
 
-//   return (
-//     <div>
-//       <Field label ="Enter English" onChange={setText} value={text} />
-//       <Languages language={language} onLanguageChange={setLanguage} />
-//       <hr />
-//       <Translate text={text} language={language} />
-//     </div>
-//   );
-// }
-// //
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-class Test extends React.Component {
-  render() {
-    return <h1>Hello World!</h1>;
-  }
+  return (
+    <div>
+      <Field label ="Enter English" onChange={setText} value={text} />
+      <Languages language={language} onLanguageChange={setLanguage} />
+      <hr />
+      <Translate text={text} language={language} />
+    </div>
+  );
 }
-
-ReactDOM.render(<Test />, document.getElementById('root'));
+//
 
 
---
+++ jsx/src/index.js
@@ -0,0 +1,9 @@
+// Import the React and ReactDOM libraries
+import React from  'react';
+import ReactDOM from 'react-dom';
+
+// Creat a react cmponent
+
+
+// Take the react component and show it on the screen
+
