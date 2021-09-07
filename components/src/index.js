17fdf5fa5a93e11b432ac5b2acf0e10ce93d4a0a
Heino <heinoduplessis@gmail.com>
2021-09-07 09:48:35 +0200

    07/09/21

---
+++ components/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>Hi daar!</div>
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
