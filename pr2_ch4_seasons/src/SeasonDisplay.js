12ee059795a2f11b98b90597c909d0930ee7bca6
Heino <heinoduplessis@gmail.com>
2021-09-08 10:55:27 +0200

    08/09/21

---
+++ pr2_ch4_seasons/src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import  ReactDOM from 'react-dom';
+
+const SeasonDisplay = () => {
+    return <div>Season Displays</div>;
+};
+export default SeasonDisplay;
---
+++ pr2_ch4_seasons/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        position => console.log(position),
+        err => console.log(err) 
+    )
+
+    return <div>
+        Hi daar!..
+    </div>
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
