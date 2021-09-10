78359ed8bf98354a5e5079da842fca75b8aa8822
Heino <heinoduplessis@gmail.com>
2021-09-10 13:02:57 +0200

    10/09/21

--- pr2_ch7_pics/src/components/SearchBar.js
+++ pr2_ch7_pics/src/components/SearchBar.js
@@ -3,16 +3,10 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-  onFormSubmit(event) {
-    event.preventDefault();
-
-    console.log(this.state.term);
-  }
-
   render() {
     return (
       <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+        <form className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
