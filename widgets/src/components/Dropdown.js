e714ad522041d6cb3369f110ec83e0c6322dc2f2
Heino <heinoduplessis@gmail.com>
2021-09-15 08:18:09 +0200

    15/09/21

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    document.body.addEventListener('click', (event) => {
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+
+      setOpen(false);
+    });
+  }, []);
+
   const renderedOptions = options.map((option) => {
+    if (option.value === selected.value) {
+      return null;
+    }
+
     return (
       <div
         key={option.value}
@@ -14,13 +31,18 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div
+          onClick={() => setOpen(!open)}
+          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+        >
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? 'visible transition' : ''}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
