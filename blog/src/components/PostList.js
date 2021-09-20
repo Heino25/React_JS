f99bb011cf3e0a50dd09fc966f35dd2d3ed734a6
Heino <heinoduplessis@gmail.com>
2021-09-20 09:03:57 +0200

    20/09/21

---
+++ blog/src/components/PostList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class PostList extends React.Component {
+    render() { 
+        return <div ClassName="ui container"><PostList /></div>
+    };
+};
+
+export default App;
