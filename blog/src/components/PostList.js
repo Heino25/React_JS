d108a5dd726a61532199acf61517d55a42ad5bea
Heino <heinoduplessis@gmail.com>
2021-09-20 09:11:09 +0200

    20/09/21

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
-    render() { 
-        return <div ClassName="ui container"><PostList /></div>
-    };
-};
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
-export default App;
+  render() {
+    return <div>Post List</div>;
+  }
+}
+
+export default connect(
+  null,
+  { fetchPosts }
+)(PostList);
